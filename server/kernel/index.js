a30f5d5d63a09dd9bebcf2c04d5d86ddaa38293d
ohabdev <ohabdev@gmail.com>
2021-01-30 01:07:28 +0600

    app server listening from kernel done

---
+++ server/kernel/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+class Kernel {
+    startHttpServer() {
+        const app = http.createServer(this.app);
+        app.listen(process.env.PORT, null, () => {
+            // TODO - load env from config
+            console.log('Express server listening on %d, in %s mode', process.env.PORT, process.env.NODE_ENV || 'development');
+        });
+    }
+}
+
+function kernelFactory(config) {
+    const kernel = new Kernel(config);
+    return kernel;
+}
+  
+module.exports = kernelFactory;
--- server/www.js
+++ server/www.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = require('./app');
 const path = require('path');
 
-app.app.use('/docs', express.static(path.join(__dirname, '..', 'apidocs')));
+// app.app.use('/docs', express.static(path.join(__dirname, '..', 'apidocs')));
 
 app.startHttpServer();
