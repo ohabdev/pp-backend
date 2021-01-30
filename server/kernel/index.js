393f8487bcd76886a9c951dd7d8dca86da321256
Ohab Riaz <kkohab494@gmail.com>
2021-01-30 18:37:26 +0600

    clear

--- server/kernel/index.js
+++ server/kernel/index.js
@@ -1,11 +1,9 @@
 const http = require('http');
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-=======
 const _ = require('lodash');
->>>>>>> b4e6fc970906d9d49b6121e29fc1d8be71629edd
+const mongoose = require('mongoose');
 
 class Kernel {
+    
     startHttpServer() {
         const app = http.createServer(this.app);
         app.listen(process.env.PORT, null, () => {
