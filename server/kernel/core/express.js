5c78d34a7e7d459dc8c061cca73098aadf820948
Ohab Riaz <kkohab494@gmail.com>
2021-01-31 13:39:55 +0600

    core express setup done

--- server/kernel/core/express.js
+++ server/kernel/core/express.js
@@ -1,7 +1,13 @@
-const express =  require('express');
-const path =  require('path');
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 const SwigEngine = require('swig').Swig;
+const info = require('../../../package.json');
 const nconf = require('nconf');
+const session  = require('express-session');
+const MongoStore =   require('connect-mongo')(session);
 const cors = require('cors');
 
 const swig = new SwigEngine();
@@ -12,6 +18,7 @@ exports.config = {
     publicPath: path.resolve('./public')
 };
 
+// Expose app
 exports.core = (kernel) => {
     kernel.addProp('app', express());
     kernel.app.engine('swig', swig.renderFile);
@@ -23,6 +30,40 @@ exports.core = (kernel) => {
     kernel.app.locals.baseUrl = nconf.get('baseUrl');
 
     process.env.ALLOW_CORS && kernel.app.use(cors());
-    
+    kernel.app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    kernel.app.use(bodyParser.json());
+    kernel.app.use(methodOverride());
+    if (process.env.NODE_ENV === 'production') {
+        // log only 4xx and 5xx responses to console
+        kernel.app.use(morgan('dev', {
+            skip(req, res) { 
+                return res.statusCode < 400
+            }
+        }));
+    } else {
+        kernel.app.use(morgan('dev'));
+    }
+
+    kernel.app.use(express.static(this.config.publicPath));
+    kernel.app.use(session({
+        secret: 'foo',
+        resave: false, //don't save session if unmodified
+        saveUninitialized: false, // don't create session until something stored
+        store: new MongoStore({
+            url: process.env.MONGO_URI,
+            autoRemove: 'native', //  Remove expired sessions
+            ttl: 14 * 24 * 60 * 60 // = 14 days. Default
+        })
+    }));
+
+    kernel.app.get('/api-author', (req, res) => {
+        res.status(200).send({
+            author: 'Ohab Riaz <ohabdev@gmaill.com>',
+            appName: info.name,
+            version: info.version,
+        })
+    })
 
 } 
