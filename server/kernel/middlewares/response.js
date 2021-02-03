77c487352100920ca762421febef01bba838cb50
Ohab Riaz <kkohab494@gmail.com>
2021-02-03 16:09:00 +0600

    system config public api language list and default language api done

--- server/kernel/middlewares/response.js
+++ server/kernel/middlewares/response.js
@@ -1,7 +1,26 @@
 
 /* eslint no-return-assign: 0 */
 
-exports.success  = fields => (req, res) => {
-    console.log("_----------------->>>>", res);
-    return res;
+// App API response middleware
+exports.success = fields => (req, res) => {
+
+    const httpCode = res.locals.httpCode || 200;
+    const code = res.locals.code || httpCode;
+    const message = res.locals.message || 'OK';
+    
+    const newFields = [].concat(fields || 'data');
+    let result = {};
+    
+    if (newFields.length === 1) {
+        result = res.locals[newFields[0]];
+    } else {
+        newFields.forEach(f => result[f] = res.locals[f])
+    }
+
+    res.status(httpCode).send({
+        code,
+        message,
+        data: result,
+        error: false
+    });
 }
--- server/module/system/controllers/config.controller.js
+++ server/module/system/controllers/config.controller.js
@@ -1,9 +1,30 @@
 
 
 exports.publicConfig = async (req, res, next) => {
-    console.log('====================================');
-    console.log(Middleware);
-    console.log('====================================');
-    res.locals.publicConfig = "Hello world";
-    next();
+
+    try {
+        const items = await DB.Config.find({ public: true }).exec();
+        const data = {};
+        // data setup key value 
+        items.forEach(item =>{
+            data[item.key] = item.value;
+        });
+
+        const languages = await DB.I18nLanguage.find({ isActive: true });
+        // find default language
+        const defaultLanguage = languages.filter(lang => lang.isDefault).map(lang => lang.key);
+        // language object filter & default language set
+        data.i18n = {
+            languages: languages.map(lang => ({
+                key: lang.key,
+                name: lang.name,
+            })),
+            defaultLanguage: defaultLanguage && defaultLanguage.length ? defaultLanguage[0] : 'en'
+        }
+
+        res.locals.publicConfig = data;
+        next()
+    } catch (e) {
+        next();
+    }
 }
